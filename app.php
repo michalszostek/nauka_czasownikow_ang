fa40a30d20052ff319f157a302aa261c648cd756
michalszostek <szostek.mike@gmail.com>
2019-04-13 10:29:25 +0200

    insert vebs to db

--- app.php
+++ app.php
@@ -14,9 +14,8 @@ try {
 }
 
 
-$verb = rand_verb($conn);
-
-function rand_verb($conn) {
+function rand_verb($conn)
+{
     $id = rand(1, 3);
     $verb = select_verb($conn, $id);
     return $verb;
@@ -30,4 +29,34 @@ function select_verb($conn, $id)
     $query->execute(array(':id' => $id));
     $row = $query->fetch(PDO::FETCH_NUM);
     return $row;
+}
+
+
+
+$maxID = 3;
+$ile_wylosowac = 2;
+$ile_juz_wylosowano = 0;
+
+for ($i = 1; $i <= $ile_wylosowac; $i++) {
+    do {
+        $liczba = rand(1, $maxID);
+        $losowanie_ok = true;
+
+        for ($j = 1; $j <= $ile_juz_wylosowano; $j++) {
+            //czy liczba nie zostala juz wczesniej wylosowana?
+            if ($liczba == $wylosowane[$j]) $losowanie_ok = false;
+        }
+
+        if ($losowanie_ok == true) {
+            //mamy unikatowa liczbe, zapiszmy ja do tablicy
+            $ile_juz_wylosowano++;
+            $wylosowane[$ile_juz_wylosowano] = $liczba;
+        }
+    } while ($losowanie_ok != true);
+}
+
+// ZOBACZ REZULTATY LOSOWANIA
+echo "Wylosowane numery: ";
+for ($i = 1; $i <= $ile_wylosowac; $i++) {
+    echo $wylosowane[$i] . " ";
 }
---
+++ verb.php
@@ -0,0 +1,44 @@
+<?php
+
+$servername = 'localhost';
+$dbname = 'verbs';
+$username = 'root';
+$password = '';
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=" . $dbname, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->exec("SET CHARACTER SET utf8");
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+
+$file = fopen('verbs.php', 'r');
+
+while (!feof($file)) {
+    $get = fgets($file);
+    $verb = explode(',', $get);
+
+
+
+    // $sql = 'INSERT INTO `verbs`(`infinitive`, `tense`, `participle`, `polish`) VALUES (:a, :b, :c, :d)';
+    // $query = $conn->prepare($sql);
+    // $query->execute((array(
+        // ':a' => $verb[0],
+        // ':b' => $verb[1],
+        // ':c' => $verb[2],
+        // ':d' => $verb[3]
+    // )));
+
+    $query = 'INSERT INTO `verbs`(`infinitive`, `tense`, `participle`, `polish`) VALUES (:a, :b, :c, :d)';
+    $query = $conn->prepare($query);
+    var_dump($query);
+    $query->execute(array(
+        ':a' => $verb[0],
+        ':b' => $verb[1],
+        ':c' => $verb[2],
+        ':d' => $verb[3]
+    ));
+
+}
