530859bbc4c7c79647a392b782773b10c68f90f3
michalszostek <szostek.mike@gmail.com>
2019-06-09 14:25:16 +0200

    previous

--- next.php
+++ next.php
@@ -3,14 +3,16 @@ include_once('verbs.php');
 include_once('db_conn.php');
 
 $id = $_SESSION['round'];
-$id++;
 
-$query = 'UPDATE `round` SET `infinitive`= :a,`tense`=:b,`participle`=:c WHERE id = :d';
-$query = $conn->prepare($query);
-$query->execute(array(
-    ':a' => $_GET['infinitive'],
-    ':b' => $_GET['tense'],
-    ':c' => $_GET['participle'],
-    ':d' => $id
-));
+if ($_GET['infinitive'] !== '' && $_GET['tense'] !== '' && $_GET['participle'] !== '') {
+    $query = 'UPDATE `round` SET `infinitive`= :a,`tense`=:b,`participle`=:c WHERE id = :d';
+    $query = $conn->prepare($query);
+    $query->execute(array(
+        ':a' => $_GET['infinitive'],
+        ':b' => $_GET['tense'],
+        ':c' => $_GET['participle'],
+        ':d' => $id
+    ));
+}
+
 header("Location: index.php");
